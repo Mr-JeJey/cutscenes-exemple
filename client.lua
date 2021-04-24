484ef0bad0e9550e64bc9b28dfa8ccea86d84b3b
Mr-JeJey <78312187+Mr-JeJey@users.noreply.github.com>
2021-04-25 00:51:10 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -4,7 +4,7 @@ local function StartLaScene()
 	local plyrId = PlayerPedId()
 	local playerClone = ClonePed_2(plyrId, 0.0, false, true, 1)
 
-        RequestCutscene('mp_intro_mcs_13', 8) 
+        RequestCutscene('mp_intro_mcs_10_a5', 8) 
 	
         while not (HasCutsceneLoaded()) do
             Wait(0)
@@ -20,8 +20,17 @@ local function StartLaScene()
 
 	SetCutsceneEntityStreamingFlags('MP_1', 0, 1)
 	RegisterEntityForCutscene(plyrId, 'MP_1', 0, GetEntityModel(plyrId), 64)
+
+	SetCutsceneEntityStreamingFlags('MP_2', 0, 1)
+	RegisterEntityForCutscene(plyrId, 'MP_2', 3, GetEntityModel(plyrId), 64)
+
+	SetCutsceneEntityStreamingFlags('MP_3', 0, 1)
+	RegisterEntityForCutscene(plyrId, 'MP_3', 3, GetEntityModel(plyrId), 64)
+
+	SetCutsceneEntityStreamingFlags('MP_4', 0, 1)
+	RegisterEntityForCutscene(plyrId, 'MP_4', 3, GetEntityModel(plyrId), 64)
     
-	RequestCollisionAtCoord(1273.908, -1715.918, 54.77644)
+	RequestCollisionAtCoord(-60.86603, -1520.641, 33.44114)
 
 	Wait(10)
 	StartCutscene(0)
